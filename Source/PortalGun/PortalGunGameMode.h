8c869ff8b27bb7db6cbc5d5b97637ca2ff634857
flecona <flecona@outlook.com>
2023-10-08 17:45:07 -0600

    Initial upload

---
+++ Source/PortalGun/PortalGunGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PortalGunGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APortalGunGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APortalGunGameMode();
+};
+
+
+
